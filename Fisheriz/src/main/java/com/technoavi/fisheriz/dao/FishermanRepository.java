364658ae7f7f039f20e5545f1037d61efd17e4c7
Avinash Srivastava <avs0949@gmail.com>
2022-11-21 23:45:07 +0530

    g branch

--- Fisheriz/src/main/java/com/technoavi/fisheriz/dao/FishermanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.technoavi.fisheriz.dao;
-
-import com.technoavi.fisheriz.models.FisherMan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FishermanRepository extends JpaRepository<FisherMan,Integer > {
-}
